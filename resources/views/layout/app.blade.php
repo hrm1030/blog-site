2514554d813c64d553aa8133f486109e6e270b83
hrm <hrm.2021@outlook.com>
2021-07-01 06:28:42 -0700

    update

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -48,6 +48,24 @@
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 }
             });
+            toastr.options = {
+                "closeButton": true,
+                "debug": false,
+                "newestOnTop": false,
+                "progressBar": false,
+                "positionClass": "toast-top-right",
+                "preventDuplicates": false,
+                "onclick": null,
+                "showDuration": "300",
+                "hideDuration": "1000",
+                "timeOut": "5000",
+                "extendedTimeOut": "1000",
+                "showEasing": "swing",
+                "hideEasing": "linear",
+                "showMethod": "fadeIn",
+                "hideMethod": "fadeOut"
+                };
+
         </script>
 		<!--end::Page Scripts-->
 	</head>
